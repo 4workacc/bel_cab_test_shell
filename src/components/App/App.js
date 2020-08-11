57f66535f6155991ec6d6ff9fa3f2c3a41ee6f91
azenkurt <azenkurit@gmail.com>
2020-08-11 16:16:54 +0300

    qwe

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import Test1 from '../Test1/Test1';
+
 const App = () => {
   return (
     <div className = 'App'>
-      App
+        <Test1 />
     </div>
   )
 }
