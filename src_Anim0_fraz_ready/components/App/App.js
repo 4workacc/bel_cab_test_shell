4fe28bbc37914d86c87bf34205b019f8814df11b
azenkurt <azenkurit@gmail.com>
2020-08-11 14:07:04 +0300

    anim ready, clear shell

--- src_Anim0_fraz_ready/components/App/App.js
+++ src_Anim0_fraz_ready/components/App/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import Anim0 from '../Anim0/Anim0.js';
+
 const App = () => {
   return (
     <div className = 'App'>
-      App
+      <Anim0 />
     </div>
   )
 }
